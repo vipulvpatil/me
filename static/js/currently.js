7cfe0d2f1a32145c3b7438adc272b606ba7b1d24
Vipul V Patil <vipulvpatil@gmail.com>
2024-07-10 13:08:46 +0000

    updated intro message and randomly selecting my current status

--- static/js/currently.js
+++ static/js/currently.js
@@ -1,5 +1,21 @@
-const test = () => {
-  console.log("testing")
+const displayCurrently = () => {
+  console.log("READY")
+  var activityElements = [
+    document.getElementById("games"),
+    document.getElementById("code"),
+    document.getElementById("football"),
+    document.getElementById("family"),
+    document.getElementById("sleep"),
+  ]
+
+  activityElements.forEach( element => {
+    element.classList.add("unselected")
+  })
+
+  const currently = activityElements[Math.floor(Math.random()*activityElements.length)]
+  currently.classList.remove("unselected")
 }
 
-test()
+window.addEventListener('DOMContentLoaded',function () {
+  displayCurrently();
+})
